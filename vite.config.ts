8e5edfd1d66904da7a22522bc7b8b791187717fa
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-09-04 22:57:37 +0900

    chore: emotion 설치 및 관련 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxImportSource: '@emotion/react'
+  })],
   resolve: {
     alias: [
       {
