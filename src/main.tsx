47811a42d082082b3efc14c13aeb3acd5a411aed
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-09-04 23:20:11 +0900

    chore: 사용할 라이브러리 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react'
-  })],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react'
+    })
+  ],
   resolve: {
     alias: [
       {
