ec8b7e7e4185ee71ef115e816bf2bded7a64d8f3
juyeon-park <lpjy1107@gmail.com>
2023-09-08 16:08:59 +0900

    feat: header ui 1차 완성

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -14,35 +14,34 @@ import {
   Avatar,
   MenuDivider
 } from '@chakra-ui/react';
-import { SearchIcon } from '@chakra-ui/icons';
+import { BellIcon, SearchIcon } from '@chakra-ui/icons';
 import logo from '@/assets/img/logo.png';
 
 const Header = () => {
   return (
     <>
-      <Box bg={'gray.100'} px={3}>
-        <Flex alignItems={'center'} justifyContent={'space-between'}>
-          <Box>
-            <Image src={logo} alt="logo" />
-          </Box>
-          <InputGroup>
+      <Box h="3rem" px={3}>
+        <Flex gap={4} alignItems={'center'} h="100%" justifyContent={'space-between'}>
+          <Image h="2rem" src={logo} alt="logo" />
+          <InputGroup w="30rem">
             <Input
-              borderRadius="10px"
-              borderColor="#E2E8F0"
               size="sm"
+              borderRadius="10px"
+              borderColor=" var(--blue01)"
               variant="outline"
               placeholder="사용자 이름을 입력해주세요"
+              _placeholder={{ opacity: 1, color: 'var(--gray03)' }}
             />
-            <InputRightElement>
+            <InputRightElement h="2rem">
               <SearchIcon />
             </InputRightElement>
           </InputGroup>
           <Stack direction={'row'} spacing={3}>
-            <Button>알림</Button>
+            <BellIcon w={6} h={6} />
             <Menu>
               <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
                 <Avatar
-                  size={'sm'}
+                  size={'xs'}
                   src={
                     'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
                   }
