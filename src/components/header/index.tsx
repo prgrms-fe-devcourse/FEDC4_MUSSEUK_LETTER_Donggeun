aee91e8c20ff57e33f75612639b30906322c14f5
juyeon-park <lpjy1107@gmail.com>
2023-09-10 03:38:33 +0900

    feature: header ui, menu dropdown 완성

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,62 +1,66 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Image,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Stack,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Avatar,
-  MenuDivider
-} from '@chakra-ui/react';
-import { BellIcon, SearchIcon } from '@chakra-ui/icons';
-import logo from '@/assets/img/logo.png';
+import { Box, Button, Flex, Image, Stack, Menu, MenuButton, MenuList, MenuItem, Avatar, Icon } from '@chakra-ui/react';
 
-const Header = () => {
+import { BellIcon, SearchIcon } from '@chakra-ui/icons';
+import logo from '@/assets/images/logo.png';
+import defaultProfile from '@/assets/images/musseuk_hood.png';
+import InputField from '@/components/Header/components/Input';
+import { useNavigate } from 'react-router-dom';
+
+type HeaderProps = {
+  profileImg?: string;
+  userId: string;
+};
+
+const Header = ({ profileImg, userId }: HeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleMenuItem = (path: string) => {
+    navigate(path);
+  };
   return (
-    <>
-      <Box h="3rem" px={3}>
-        <Flex gap={4} alignItems={'center'} h="100%" justifyContent={'space-between'}>
-          <Image h="2rem" src={logo} alt="logo" />
-          <InputGroup w="30rem">
-            <Input
-              size="sm"
-              borderRadius="10px"
-              borderColor=" var(--blue01)"
-              variant="outline"
-              placeholder="사용자 이름을 입력해주세요"
-              _placeholder={{ opacity: 1, color: 'var(--gray03)' }}
-            />
-            <InputRightElement h="2rem">
-              <SearchIcon />
-            </InputRightElement>
-          </InputGroup>
-          <Stack direction={'row'} spacing={3}>
-            <BellIcon w={6} h={6} />
-            <Menu>
-              <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
-                <Avatar
-                  size={'xs'}
-                  src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
-              </MenuButton>
-              <MenuList>
-                <MenuItem>프로필</MenuItem>
-                <MenuDivider />
-                <MenuItem>비밀번호 변경</MenuItem>
-              </MenuList>
-            </Menu>
-          </Stack>
-        </Flex>
-      </Box>
-    </>
+    <Box h={12} px={3}>
+      <Flex gap={4} alignItems={'center'} h="100%" justifyContent={'space-between'}>
+        <Image h={8} cursor="pointer" src={logo} alt="logo" onClick={() => handleMenuItem('/')} />
+        <InputField
+          icon={<Icon as={SearchIcon} />}
+          mw={'35rem'}
+          inputProps={{
+            placeholder: '사용자 이름을 입력해 주세요',
+            size: 'sm',
+            fontSize: '14px',
+            fontWeight: 'bold'
+          }}
+        />
+        <Stack direction={'row'} spacing={3}>
+          <BellIcon w={6} h={6} cursor={'pointer'} />
+          <Menu autoSelect={false}>
+            <MenuButton as={Button} rounded={'full'} variant={'link'} cursor={'pointer'} minW={0}>
+              <Avatar size={'xs'} src={profileImg ?? defaultProfile} />
+            </MenuButton>
+            <MenuList minW={36} p={0}>
+              <MenuItem
+                borderTopLeftRadius={5}
+                borderTopRightRadius={5}
+                h={7}
+                justifyContent={'center'}
+                _hover={{ bg: 'var(--gray01)' }}
+                onClick={() => handleMenuItem(`/profile/${userId}`)}>
+                프로필
+              </MenuItem>
+              <MenuItem
+                borderBottomLeftRadius={5}
+                borderBottomRightRadius={5}
+                h={7}
+                justifyContent={'center'}
+                _hover={{ bg: 'var(--gray01)' }}
+                onClick={() => handleMenuItem(`/changePassword/${userId}`)}>
+                비밀번호 변경
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Stack>
+      </Flex>
+    </Box>
   );
 };
 
