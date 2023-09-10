1b63e89f58da5c999dc269cf72de7a7bcb19a4ee
Howon Shin <shinhw371@gmail.com>
2023-09-10 23:32:07 +0900

    design: 텍스트 분리 기준 단어로 수정

--- src/pages/Post/components/AnnouncementText.tsx
+++ src/pages/Post/components/AnnouncementText.tsx
@@ -2,7 +2,7 @@ import { Heading, HeadingProps } from '@chakra-ui/react';
 
 const AnnouncementText = ({ children, ...props }: HeadingProps) => {
   return (
-    <Heading size="xl" maxW="45rem" {...props}>
+    <Heading size="xl" maxW="45rem" wordBreak={'keep-all'} {...props}>
       {children}
     </Heading>
   );
--- src/pages/Post/components/DescriptionText.tsx
+++ src/pages/Post/components/DescriptionText.tsx
@@ -2,7 +2,7 @@ import { Text, TextProps } from '@chakra-ui/react';
 
 const DescriptionText = ({ children, ...props }: TextProps) => {
   return (
-    <Text maxW="40rem" fontSize="lg">
+    <Text maxW="40rem" fontSize="lg" wordBreak="keep-all">
       {children}
     </Text>
   );
