021990e87aa6da253e36a722a588a246d943037d
from1to2 <ngcd12345@gmail.com>
2023-09-10 12:02:14 +0900

    style: newpost page UI 구현

--- src/pages/Newpost/index.tsx
+++ src/pages/Newpost/index.tsx
@@ -1,22 +1,29 @@
-import { Textarea, Box, Text, Input, Heading } from '@chakra-ui/react';
+import { Flex, Button, Image, Textarea, Box, Text, Input } from '@chakra-ui/react';
+import Musseuk from '@/assets/images/musseuk_semicolon.png';
 
 const NewPost = () => {
   return (
     <>
-      <Text textAlign="center" fontSize="1.5rem" color="black">
-        머쓱이 이름
-      </Text>
-      <Input w="50%" placeholder="머쓱이 이름을 작성해 주세요!" size="md" />
-      <Text textAlign="center" fontSize="1.5rem" color="black">
-        머쓱이 테마
-      </Text>
-      <Box border="1px" h="35rem" w="50%" p={4}>
-        This is the Box
+      <Box margin="auto" w="50%" textAlign="center">
+        <Text textAlign="center" fontSize="1.5rem" color="black">
+          머쓱이 이름
+        </Text>
+        <Input placeholder="머쓱이 이름을 작성해 주세요!" size="md" />
+        <Text textAlign="center" fontSize="1.5rem" color="black">
+          머쓱이 테마
+        </Text>
+        <Box margin="auto" border="1px" h="35rem" p={4}>
+          <Image src={Musseuk} alt="Dan Abramov" />
+        </Box>
+        <Text textAlign="center" fontSize="1.5rem" color="black">
+          편지를 쓸 사람들에게 보여줄 멘트
+        </Text>
+        <Textarea placeholder="머쓱이에 대한 소개를 작성해주세요" />
+        <Flex justifyContent="space-between">
+          <Button colorScheme="gray">뒤로가기</Button>
+          <Button colorScheme="whatsapp">생성하기</Button>
+        </Flex>
       </Box>
-      <Text textAlign="center" fontSize="1.5rem" color="black">
-        편지를 쓸 사람들에게 보여줄 멘트
-      </Text>
-      <Textarea w="50%" placeholder="머쓱이에 대한 소개를 작성해주세요" />
     </>
   );
 };
