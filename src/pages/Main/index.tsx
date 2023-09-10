17aaa54ab89b2e202af6207b2ecaed548f68a30d
juyeon-park <lpjy1107@gmail.com>
2023-09-11 01:58:00 +0900

    feat: header 로그인 전/후 UI 변경 구현 (#35)
    * feat: header 로그인 전/후 UI 변경 구현
    
    * refactor: header button 컴포넌트화 + 코드리뷰 반영

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/Header';
 const Main = () => {
   return (
     <>
-      <Header userId="frong123" />
+      <Header userId="frong" />
       <h1>Main page</h1>
     </>
   );
