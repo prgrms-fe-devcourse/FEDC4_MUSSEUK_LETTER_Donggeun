e9d8a330eea26c37cf8b0f205985051cdeef9ce6
from1to2 <ngcd12345@gmail.com>
2023-09-10 13:07:55 +0900

    style: mainpage UI 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from 'react';
 import Musseuk from '@/assets/images/musseuk_semicolon.png';
+import Museeukhood from '@/assets/images/musseuk_hood.png';
+import rightarrow from '@/assets/images/rightarrow.png';
 import {
   ButtonGroup,
   CardFooter,
@@ -32,16 +34,18 @@ const Main = () => {
 
   return (
     <>
-      <Box p="3rem" maxW="32rem">
+      <Box w="100%" bg="lightgreen" p="3rem">
         <Heading mb={4}>데브코스 익명 편지 전송 서비스</Heading>
         <br></br>
-        <Text fontSize="xl">
-          팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요? 다른 사람의 머쓱이에게 편지를 남겨보세요!
-        </Text>
+        <Text fontSize="xl">팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요?</Text>
+        <Text fontSize="xl">다른 사람의 머쓱이에게 편지를 남겨보세요!</Text>
         <br></br>
         <Text fontSize="xl"> 또는, 당신의 머쓱이를 만들어서 공유해보세요! </Text>
-        <Button size="lg" colorScheme="green" mt="24px">
-          나만의 머쓱이 만들기
+        <Button display="flex" m="auto" size="lg" colorScheme="green" mt="3.5rem">
+          <Image boxSize="2.5rem" src={Museeukhood} alt="Museeukhood" />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;나만의 머쓱이
+          만들기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Image src={rightarrow} alt="rightarrow" />
         </Button>
       </Box>
 
