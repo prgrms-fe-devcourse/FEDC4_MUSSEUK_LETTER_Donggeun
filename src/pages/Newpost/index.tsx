9bce78f321f964761be73d8460772007092be297
from1to2 <ngcd12345@gmail.com>
2023-09-11 15:54:11 +0900

    style: header 추가, 배경색 변경

--- src/pages/Newpost/index.tsx
+++ src/pages/Newpost/index.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/header';
 import { Flex, Button, Image, Textarea, Box, Text, Input } from '@chakra-ui/react';
 import Musseuk from '@/assets/images/musseuk_semicolon.png';
 import { useNavigate } from 'react-router-dom';
@@ -6,31 +7,34 @@ const NewPost = () => {
   const navigate = useNavigate();
   return (
     <>
-      <Box p="2rem" margin="auto" w="34%" textAlign="center">
-        <Text marginBottom="1rem" textAlign="left" fontSize="1.5rem" color="black">
-          머쓱이 이름
-        </Text>
-        <Input marginBottom="2rem" placeholder="머쓱이 이름을 작성해 주세요!" size="md" />
-        <Text marginBottom="1rem" textAlign="left" fontSize="1.5rem" color="black">
-          머쓱이 테마
-        </Text>
-        <Box margin="auto" border="1px" h="35rem" p={4}>
-          <Image src={Musseuk} alt="Dan Abramov" />
+      <Header></Header>
+      <Box bgColor="color(display-p3 0.9765 0.9765 0.9569);">
+        <Box p="2rem" margin="auto" w="34%" textAlign="center">
+          <Text marginBottom="1rem" textAlign="left" fontSize="1.5rem" color="black">
+            머쓱이 이름
+          </Text>
+          <Input marginBottom="2rem" placeholder="머쓱이 이름을 작성해 주세요!" size="md" />
+          <Text marginBottom="1rem" textAlign="left" fontSize="1.5rem" color="black">
+            머쓱이 테마
+          </Text>
+          <Box margin="auto" border="1px" h="35rem" p={4}>
+            <Image src={Musseuk} alt="Dan Abramov" />
+          </Box>
+          <Text marginBottom="1rem" marginTop="2rem" textAlign="left" fontSize="1.5rem" color="black">
+            편지를 쓸 사람들에게 보여줄 멘트
+          </Text>
+          <Textarea size="lg" marginBottom="4rem" placeholder="머쓱이에 대한 소개를 작성해주세요" />
+          <Flex justifyContent="space-between">
+            <Button
+              onClick={() => {
+                navigate('/');
+              }}
+              colorScheme="gray">
+              뒤로가기
+            </Button>
+            <Button colorScheme="primary">생성하기</Button>
+          </Flex>
         </Box>
-        <Text marginBottom="1rem" marginTop="2rem" textAlign="left" fontSize="1.5rem" color="black">
-          편지를 쓸 사람들에게 보여줄 멘트
-        </Text>
-        <Textarea size="lg" marginBottom="4rem" placeholder="머쓱이에 대한 소개를 작성해주세요" />
-        <Flex justifyContent="space-between">
-          <Button
-            onClick={() => {
-              navigate('/');
-            }}
-            colorScheme="gray">
-            뒤로가기
-          </Button>
-          <Button colorScheme="primary">생성하기</Button>
-        </Flex>
       </Box>
     </>
   );
