17a7ab856b6fd90eca8e366280ab8ac736bfa51f
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-09-12 02:21:05 +0900

    refactor: InputField 컴포넌트 Props 리팩토링

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -37,6 +37,7 @@ const SignIn = () => {
         onSuccess: () => navigate(links.main),
         onError: (error) => {
           const errorMessage = typeof error.response?.data === 'string' ? error.response?.data : '';
+          console.log(error);
           setError('email', { type: 'server', message: errorMessage });
           setError('password', { type: 'server', message: errorMessage });
         }
@@ -61,25 +62,21 @@ const SignIn = () => {
         Welcome to 머쓱레터 <EmailIcon />
       </Flex>
       <InputField
+        {...register('email')}
+        id="email"
+        type="email"
         label="Email"
         error={errors.email}
-        inputProps={{
-          id: 'email',
-          type: 'email',
-          placeholder: '이메일을 입력해주세요'
-        }}
-        registerProps={register('email')}
+        placeholder="이메일을 입력해주세요"
       />
       <InputField
+        {...register('password')}
+        id="password"
+        type="password"
         label="Password"
         error={errors.password}
-        inputProps={{
-          id: 'password',
-          type: 'password',
-          placeholder: '비밀번호를 입력해주세요',
-          maxLength: 30
-        }}
-        registerProps={register('password')}
+        placeholder="비밀번호를 입력해주세요"
+        maxLength={30}
       />
       <Button type="submit" mt="6" w="100%" colorScheme="primary">
         Sign in
