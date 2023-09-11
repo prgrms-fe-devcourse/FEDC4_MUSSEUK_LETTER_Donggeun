d38dbbb1ec82dba317efae1f4ee2b54756197f53
Eosdia <lumimomen@gmail.com>
2023-09-11 13:49:57 +0900

    feat: profileBar ui Component

---
+++ src/pages/Profile/components/ProfileBar.tsx
@@ -0,0 +1,38 @@
+import { VStack, HStack, Avatar, Button, Text, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { EmailIcon, ChatIcon } from '@chakra-ui/icons';
+import hoodMusseuk from '@/assets/images/musseuk_hood.png';
+
+const UserInfoField = ({ icon, type, value }) => {
+  return (
+    <FormControl>
+      <HStack my={6}>
+        <FormLabel my={0}>{icon}</FormLabel>
+        <Input type={type} w={'80%'} h={6} value={value} border={'none'} />
+      </HStack>
+    </FormControl>
+  );
+};
+
+const ProfileBar = ({ userName }) => {
+  return (
+    <VStack h={'100vh'} my={10} pt={4} px={6} borderRight="1px solid #B6B6B6">
+      <VStack py={4}>
+        <Avatar size={'2xl'} src={hoodMusseuk} />
+        <Button h={8} bg={'green01'} color={'white'}>
+          이미지 업로드
+        </Button>
+        <Text>{userName} 님</Text>
+      </VStack>
+      <VStack>
+        <UserInfoField icon={<EmailIcon />} type={'email'} value={'prong@gmail.com'} />
+        <UserInfoField icon={<EmailIcon />} type={'text'} value={'Nickname'} />
+        <UserInfoField icon={<ChatIcon />} type={'text'} value={'Introduce~'} />
+      </VStack>
+      <Button h={8} bg={'green01'} color={'white'}>
+        프로필 편집하기
+      </Button>
+    </VStack>
+  );
+};
+
+export default ProfileBar;
