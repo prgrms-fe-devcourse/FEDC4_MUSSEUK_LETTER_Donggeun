46c69b4e1e42056d2bdf77f8416b8f0c73d44dfd
Howon Shin <shinhw371@gmail.com>
2023-09-11 14:42:46 +0900

    design: colorScheme 사용

--- src/pages/Post/components/ListButton.tsx
+++ src/pages/Post/components/ListButton.tsx
@@ -2,7 +2,7 @@ import { Button, ButtonProps } from '@chakra-ui/react';
 
 const ListButton = ({ ...props }: ButtonProps) => {
   return (
-    <Button backgroundColor="green01" color="white" size="lg" _hover={{ backgroundColor: 'green03' }} {...props}>
+    <Button colorScheme="primary" size="lg" {...props}>
       리스트로 보기
     </Button>
   );
