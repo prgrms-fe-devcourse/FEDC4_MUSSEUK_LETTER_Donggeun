fa9f7ddc718a38ad70a87d3f859b3102f71c823f
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-09-11 17:41:36 +0900

    remove: App.tsx에서 useAuthCheckQuery를 테스트하던 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route } from './routes';
-import useAuthCheckQuery from '@/apis/queries/useAuthCheckQuery';
 
 const App = () => {
-  const { data } = useAuthCheckQuery();
-
-  console.log(data);
-
   return (
     <Router>
       <Route />
