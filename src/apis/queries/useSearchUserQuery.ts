477bb28da603eb850bd806d5787ba62cb2e77089
juyeon-park <lpjy1107@gmail.com>
2023-09-12 17:19:52 +0900

    feat: 사용자 검색 api 구현 (#61)
    * feat: 검색 api 1차 작업 중
    
    * feat: 사용자 검색 api 구현

---
+++ src/apis/queries/useSearchUserQuery.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { baseInstance } from '@/apis/instance';
+import { UserResponse } from '@/apis/types';
+import queryKey from '@/apis/queryKeys';
+
+export const getSearchResult = async (keyword: string) => {
+  const { data } = await baseInstance.get<UserResponse[]>(`/search/users/${keyword}`);
+
+  return data;
+};
+
+const useSearchUserQuery = (keyword: string) => {
+  return useQuery<UserResponse[]>({
+    queryKey: queryKey.users.search(keyword),
+    queryFn: () => getSearchResult(keyword)
+  });
+};
+
+export default useSearchUserQuery;
