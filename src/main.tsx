9925d6d1ae1f6c132a784ca340e245af39949e23
bbearcookie <50488780+bbearcookie@users.noreply.github.com>
2023-09-12 23:27:58 +0900

    chore: react-toastify 설치

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import theme from '@/theme/index.tsx';
 import Fonts from '@/theme/font.tsx';
 import App from './App.tsx';
@@ -15,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <ChakraProvider theme={theme}>
         <App />
         <Fonts />
+        <ToastContainer />
         <ReactQueryDevtools initialIsOpen={false} />
       </ChakraProvider>
     </QueryClientProvider>
