71fb535af7a3bc29f3c3cceda72067a5bc35b3a5
Howon Shin <shinhw371@gmail.com>
2023-09-14 13:02:11 +0900

    feat: 사용 type 재정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,36 +1,37 @@
-export interface User {
+export interface User extends UserFullName {
   _id: string;
   email: string;
+  image: string; // default: 기본 이미지
+  posts: string[]; // 포스트 id 목록
+  comments: string[]; // 댓글 id 목록
+}
+
+export interface UserFullName {
   username: string; // 사용자 실명
-  nickname: string;
-  introduce: string;
-  image: string;
+  introduce: string; // default: "안녕하세요 000입니다"
+  slackId?: string; // default: undefined
+  slackWorkspace?: 'Frontend' | 'Backend'; // default: undefined
 }
 
-export interface customUserFullName {
-  username: string;
-  nickname: string;
-  introduce: string;
-}
-
-export interface Post extends customPostTitle {
+export interface Post extends PostTitle {
   _id: string;
   comments: Comment[];
   author: User;
 }
 
-export interface customPostTitle {
-  title: string;
-  content: string;
-  musseukId: string;
+export interface PostTitle {
+  title: string; // 머쓱이 제목, default: "머쓱이"
+  content: string; // 소개글, default: ""
+  musseukImageName: string; // 머쓱이 이미지 파일 이름
 }
 
-export interface Comment extends customCommentComment {
+export interface Comment extends CommentField {
   _id: string;
 }
 
-export interface customCommentComment {
-  content: string;
-  pos: [number, number];
-  nickname: string;
+export interface CommentField {
+  content: string; // default: ""
+  pos: [number, number]; // 댓글 좌표(백분율), default: [0, 0]
+  nickname: string; // default: "익명의 머쓱이"
+  decorationImageName: string; // 장식 이미지 파일 이름, default: ""
 }
