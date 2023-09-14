1df470438ca4f344fda35e9529f16fa66d4855af
juyeon-park <lpjy1107@gmail.com>
2023-09-14 18:26:37 +0900

    feat: 편지 1개 보기 모달 UI 구현

--- src/pages/Post/components/CommentInfoModal/index.tsx
+++ src/pages/Post/components/CommentInfoModal/index.tsx
@@ -1,14 +1,48 @@
 import BasicModal from '@/components/Modal';
-import { Box, ModalCloseButton, ModalContent, ModalHeader, ModalProps, UseDisclosureReturn } from '@chakra-ui/react';
-
-const CommentInfoModal = ({ isOpen, onClose }: Pick<UseDisclosureReturn, 'isOpen' | 'onClose'> & ModalProps) => {
+import {
+  Image,
+  Box,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  UseDisclosureReturn,
+  ModalBody,
+  Text,
+  VStack
+} from '@chakra-ui/react';
+import headerImage from '@/assets/images/comment-header.png';
+import decoImage from '@/assets/images/decoration_soju1.png';
+const CommentInfoModal = ({ isOpen, onClose }: Pick<UseDisclosureReturn, 'isOpen' | 'onClose'>) => {
   return (
     <BasicModal isOpen={isOpen} onClose={onClose}>
-      <ModalContent>
-        <ModalCloseButton />
-        <ModalHeader>
-          <Box>한눈에 보기 모달</Box>
+      <ModalContent borderRadius={20}>
+        <ModalCloseButton size={'lg'} top={3} color={'white'} zIndex={1} />
+        <ModalHeader h={'4rem'} p={0} bgImage={headerImage} borderTopLeftRadius={20} borderTopRightRadius={20}>
+          <Box w={'100%'} h={'100%'} position={'relative'}>
+            <Image
+              m={'auto'}
+              w={'3rem'}
+              h={'4rem'}
+              position={'inherit'}
+              top={'2rem'}
+              alt="comment-header"
+              src={decoImage}
+            />
+          </Box>
         </ModalHeader>
+        <ModalBody p={'3rem'}>
+          <VStack gap={6} alignItems={'end'}>
+            <Box minH={'14rem'} borderRadius={6} p={'1.5rem'} bg={'bg01'}>
+              <Text lineHeight={'150%'}>
+                안녕 프롱아...디자인 너무 고생했어. 미안해 내가 디자인 감각이 없어서 도움을 줄 수 없어서 그래서 옆에서
+                응원만 했단다. 하지만 너의 디자인 감각 나는 좋은 것 같아 그니까 자신감 가져!!!!
+              </Text>
+            </Box>
+            <Text textAlign={'right'} fontWeight={'bold'} fontSize={'xl'}>
+              From. 익명의 머쓱이
+            </Text>
+          </VStack>
+        </ModalBody>
       </ModalContent>
     </BasicModal>
   );
--- src/pages/Post/components/CommentListModal/index.tsx
+++ src/pages/Post/components/CommentListModal/index.tsx
@@ -1,7 +1,7 @@
 import BasicModal from '@/components/Modal';
-import { ModalContent, ModalProps, UseDisclosureReturn } from '@chakra-ui/react';
+import { ModalContent, UseDisclosureReturn } from '@chakra-ui/react';
 
-const CommentListModal = ({ isOpen, onClose }: Pick<UseDisclosureReturn, 'isOpen' | 'onClose'> & ModalProps) => {
+const CommentListModal = ({ isOpen, onClose }: Pick<UseDisclosureReturn, 'isOpen' | 'onClose'>) => {
   return (
     <BasicModal isOpen={isOpen} onClose={onClose}>
       <ModalContent></ModalContent>
