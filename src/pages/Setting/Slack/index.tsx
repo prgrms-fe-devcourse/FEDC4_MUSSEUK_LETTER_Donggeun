00d1a95171d3b4e182ebae909c9b8441e3592a32
juyeon-park <lpjy1107@gmail.com>
2023-09-19 04:06:08 +0900

    feat: 슬랙 알림 페이지 ui 구현 및 슬랙 api 연결

--- src/pages/Setting/Slack/index.tsx
+++ src/pages/Setting/Slack/index.tsx
@@ -1,10 +1,10 @@
-import { PageTemplate } from '@/pages/Signup/templates';
+import PageTemplateWithHeader from '@/components/WhiteCard/PageTemplateWithHeader';
 import { Image, Heading, Button, FormControl, RadioGroup, Radio, VStack, Text, Stack } from '@chakra-ui/react';
 import musseuk_slack from '@/assets/images/musseuk_slack.png';
 import InputField from '@/pages/Signup/components/InputField';
 import { QuestionIcon } from '@chakra-ui/icons';
 import useGenerateSlackLinkMutation from '@/apis/mutations/useGenerateSlackLinkMutation';
-import { SubmitHandler, useForm, Controller } from 'react-hook-form';
+import { SubmitHandler, useForm, Controller, SubmitErrorHandler } from 'react-hook-form';
 import LinkField from './components/LinkField';
 import { SLACK_ID_DESCRIPTION } from '@/pages/Setting/Slack/constants/index';
 import { z } from 'zod';
@@ -29,11 +29,18 @@ const SettingSlack = () => {
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
-    mutate({ slackId: data.slackId, slackWorkspace: data.slackWorkspace });
+    mutate(
+      { slackId: data.slackId, slackWorkspace: data.slackWorkspace },
+      {
+        onSuccess: (res) => console.log(res)
+      }
+    );
   };
 
+  const onError: SubmitErrorHandler<FormValues> = (errors) => console.error(errors);
+
   return (
-    <PageTemplate onSubmit={handleSubmit(onSubmit)}>
+    <PageTemplateWithHeader onSubmit={handleSubmit(onSubmit, onError)}>
       <Image maxW="32" src={musseuk_slack} alt="머쓱이" />
       <Heading fontSize={'3xl'} textAlign="center" wordBreak={'keep-all'}>
         슬랙 인증을 통해 편지 알림을 슬랙으로 받아보세요
@@ -46,7 +53,7 @@ const SettingSlack = () => {
           render={({ field }) => (
             <RadioGroup {...field} display={'flex'} w={'100%'} gap={2} my={8}>
               <VStack gap={2} w={'100%'} alignItems={'start'}>
-                <Text>데브코스</Text>
+                <Text fontWeight={'bold'}>데브코스</Text>
                 <Stack spacing={5} direction="row">
                   <Radio colorScheme="primary" borderColor={'gray.200'} value="Frontend">
                     프론트엔드
@@ -66,6 +73,7 @@ const SettingSlack = () => {
         label="Slack Id"
         placeholder="슬랙 아이디를 입력해주세요"
         maxLength={40}
+        error={errors.slackId}
       />
       <LinkField title="노션 페이지로 이동" href={SLACK_ID_DESCRIPTION}>
         <QuestionIcon color={'primary.700'} mr={1} />
@@ -74,7 +82,7 @@ const SettingSlack = () => {
       <Button type="submit" mt="6" w="100%" colorScheme="primary">
         슬랙 인증하기
       </Button>
-    </PageTemplate>
+    </PageTemplateWithHeader>
   );
 };
 
