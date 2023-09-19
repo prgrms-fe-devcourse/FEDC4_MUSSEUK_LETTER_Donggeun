83335ab4536647918fcff671518ca7300f53689b
from1to2 <ngcd12345@gmail.com>
2023-09-19 22:57:58 +0900

    style: mainpage responsive

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,7 @@
 import Museeukhood from '@/assets/images/musseuk_hood.png';
 import rightarrow from '@/assets/images/rightarrow.png';
 import { useNavigate, Navigate } from 'react-router-dom';
-import { Heading, Box, Image, Text, Button } from '@chakra-ui/react';
+import { Heading, Box, Image, Text, Button, Center } from '@chakra-ui/react';
 import { Virtual, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -13,6 +13,7 @@ import PostCard from '@/components/PostCard';
 import useAuthCheckQuery from '@/apis/queries/useAuthCheckQuery';
 import storage from '@/utils/storage';
 import { AUTH_TOKEN } from '@/constants/storageKey';
+import { useMediaQuery } from 'react-responsive';
 
 const Main = () => {
   const navigate = useNavigate();
@@ -21,43 +22,139 @@ const Main = () => {
   // console.log(data);
   const { data: user } = useAuthCheckQuery();
 
+  const isPc = useMediaQuery({
+    query: '(min-width:1642px)'
+  });
+  const isTablet = useMediaQuery({
+    query: '(min-width : 1061px) and (max-width :1641px)'
+  });
+  const isMobile = useMediaQuery({
+    query: '(max-width:1061px)'
+  });
   return (
     <>
-      <Box w="100%" bgGradient="linear-gradient(180deg, #C6FFC1 0%, #F5FFE2 100%)" p="3rem">
-        <Image
-          top="3.3rem"
-          right="1rem"
-          position="absolute"
-          w="40rem"
-          h="30rem"
-          src={musseuk_semicolon}
-          alt="musseuk_semicolon"
-        />
-        <Heading mb={4}>데브코스 익명 편지 전송 서비스</Heading>
-        <br></br>
-        <Text fontSize="xl">팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요?</Text>
-        <Text fontSize="xl">다른 사람의 머쓱이에게 편지를 남겨보세요!</Text>
-        <br></br>
-        <Text fontSize="xl"> 또는, 당신의 머쓱이를 만들어서 공유해보세요! </Text>
-        <Button
-          onClick={() => {
-            if (!user && !storage('session').getItem(AUTH_TOKEN, null)) {
-              navigate('/signin');
-            } else {
-              navigate('/newpost');
-            }
-          }}
-          display="flex"
-          m="auto"
-          size="lg"
-          colorScheme="primary"
-          mt="3.5rem">
-          <Image boxSize="2.5rem" src={Museeukhood} alt="Museeukhood" />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;나만의 머쓱이
-          만들기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <Image src={rightarrow} alt="rightarrow" />
-        </Button>
-      </Box>
+      {isPc && (
+        <Box w="100%" bgGradient="linear-gradient(180deg, #C6FFC1 0%, #F5FFE2 100%)" p="3rem">
+          <Image
+            top="3.3rem"
+            right="1rem"
+            position="absolute"
+            w="40rem"
+            h="30rem"
+            src={musseuk_semicolon}
+            alt="musseuk_semicolon"
+          />
+          <Heading mb={4}>데브코스 익명 편지 전송 서비스</Heading>
+          <br></br>
+          <Text fontSize="xl">팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요?</Text>
+          <Text fontSize="xl">다른 사람의 머쓱이에게 편지를 남겨보세요!</Text>
+          <br></br>
+          <Text fontSize="xl"> 또는, 당신의 머쓱이를 만들어서 공유해보세요! </Text>
+          <Button
+            onClick={() => {
+              if (!user && !storage('session').getItem(AUTH_TOKEN, null)) {
+                navigate('/signin');
+              } else {
+                navigate('/newpost');
+              }
+            }}
+            display="flex"
+            m="auto"
+            size="lg"
+            colorScheme="primary"
+            mt="3.5rem">
+            <Image boxSize="2.5rem" src={Museeukhood} alt="Museeukhood" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;나만의 머쓱이
+            만들기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Image src={rightarrow} alt="rightarrow" />
+          </Button>
+        </Box>
+      )}
+      {isTablet && (
+        <Box w="100%" bgGradient="linear-gradient(180deg, #C6FFC1 0%, #F5FFE2 100%)" p="3rem">
+          <Image
+            top="3.3rem"
+            right="1rem"
+            position="absolute"
+            w="40rem"
+            h="30rem"
+            src={musseuk_semicolon}
+            alt="musseuk_semicolon"
+          />
+          <Heading mb={4}>데브코스 익명 편지 전송 서비스</Heading>
+          <br></br>
+          <Text fontSize="xl">팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요?</Text>
+          <Text fontSize="xl">다른 사람의 머쓱이에게 편지를 남겨보세요!</Text>
+          <br></br>
+          <Text fontSize="xl"> 또는, 당신의 머쓱이를 만들어서 공유해보세요! </Text>
+          <Box height="10rem"></Box>
+          <Button
+            onClick={() => {
+              if (!user && !storage('session').getItem(AUTH_TOKEN, null)) {
+                navigate('/signin');
+              } else {
+                navigate('/newpost');
+              }
+            }}
+            display="flex"
+            m="auto"
+            size="lg"
+            colorScheme="primary"
+            mt="3.5rem">
+            <Image boxSize="2.5rem" src={Museeukhood} alt="Museeukhood" />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;나만의 머쓱이
+            만들기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Image src={rightarrow} alt="rightarrow" />
+          </Button>
+        </Box>
+      )}
+      {isMobile && (
+        <Box w="100%" bgGradient="linear-gradient(180deg, #C6FFC1 0%, #F5FFE2 100%)" p="3rem">
+          <Heading fontSize="4xl" textAlign="center" mb={10}>
+            데브코스 익명 편지 전송 서비스
+          </Heading>
+          <br></br>
+          <Text textAlign="center" fontSize="xl">
+            팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요?
+          </Text>
+          <Text textAlign="center" fontSize="xl">
+            다른 사람의 머쓱이에게 편지를 남겨보세요!
+          </Text>
+          <br></br>
+          <Text textAlign="center" fontSize="xl">
+            {' '}
+            또는, 당신의 머쓱이를 만들어서 공유해보세요!{' '}
+          </Text>
+          <Box display="block" height="30%">
+            <Image
+              margin="auto"
+              top="3.3rem"
+              right="1rem"
+              w="90%"
+              h="auto"
+              src={musseuk_semicolon}
+              alt="musseuk_semicolon"
+            />
+          </Box>
+          <Button
+            onClick={() => {
+              if (!user && !storage('session').getItem(AUTH_TOKEN, null)) {
+                navigate('/signin');
+              } else {
+                navigate('/newpost');
+              }
+            }}
+            display="flex"
+            m="auto"
+            size="lg"
+            colorScheme="primary"
+            mt="3.5rem">
+            <Image boxSize="2rem" src={Museeukhood} alt="Museeukhood" />
+            &nbsp;&nbsp;&nbsp;&nbsp;나만의 머쓱이 만들기&nbsp;&nbsp;&nbsp;&nbsp;
+            <Image boxSize="0.8rem" src={rightarrow} alt="rightarrow" />
+          </Button>
+        </Box>
+      )}
       <Box bgColor="bg01" paddingTop="5rem">
         <Swiper
           breakpoints={{
