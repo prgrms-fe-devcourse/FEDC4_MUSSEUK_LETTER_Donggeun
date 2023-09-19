ab298adaa2eb761a75d20fec945a398370c90977
Eosdia <lumimomen@gmail.com>
2023-09-20 00:12:19 +0900

    feat: 사용자의 post목록을 가져오기 특정사용자의 포스트목록 api 사용

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -13,52 +13,10 @@ import {
   useMediaQuery
 } from '@chakra-ui/react';
 import { AddIcon } from '@chakra-ui/icons';
-import defaultMusseuk from '@/assets/images/musseuk_default.png';
-import hearttMusseuk from '@/assets/images/musseuk_heart.png';
-import hoodMusseuk from '@/assets/images/musseuk_hood.png';
-import labtopMusseuk from '@/assets/images/musseuk_labtop.png';
-import semicolonMusseuk from '@/assets/images/musseuk_semicolon.png';
 import ProfileBar from './components/ProfileBar';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import useUserPostListQuery from '@/apis/queries/useUserPostListQuery';
 
-const USER_NAME = '이상훈';
-const DUMMY_DATA = [
-  {
-    id: 1,
-    imgUrl: defaultMusseuk,
-    musseukName: 'musseuk1',
-    musseukContent: "hello I'm Musseuk1",
-    letter: 10
-  },
-  {
-    id: 2,
-    imgUrl: hearttMusseuk,
-    musseukName: 'musseuk2',
-    musseukContent: "hello I'm Musseuk2",
-    letter: 11
-  },
-  {
-    id: 3,
-    imgUrl: hoodMusseuk,
-    musseukName: 'musseuk3',
-    musseukContent: "hello I'm Musseuk3",
-    letter: 15
-  },
-  {
-    id: 4,
-    imgUrl: labtopMusseuk,
-    musseukName: 'musseuk4',
-    musseukContent: "hello I'm Musseuk4",
-    letter: 18
-  },
-  {
-    id: 5,
-    imgUrl: semicolonMusseuk,
-    musseukName: 'musseuk5',
-    musseukContent: "hello I'm Musseuk5",
-    letter: 18
-  }
-];
 export type AddCardProps = {
   goToNewPost: () => void;
 };
@@ -86,6 +44,8 @@ const AddCard = ({ goToNewPost }: AddCardProps) => {
 };
 
 const Profile = () => {
+  const { userId } = useParams();
+  const { data: postList } = useUserPostListQuery(userId);
   const [isSmallerThan768] = useMediaQuery('(max-width: 768px)');
   const navigate = useNavigate();
 
@@ -97,24 +57,28 @@ const Profile = () => {
     <>
       <Grid bg={'bg0101'} h="100vh" gridTemplateColumns={'1fr 3.5fr'}>
         <GridItem>
-          <ProfileBar userName={'이상훈'} />
+          <ProfileBar />
         </GridItem>
         <GridItem>
           <Stack h={64} bg={'linear-gradient(93deg, #CCFFB4 10.51%, #F8FFCF 81.79%)'} ml={6} px={6}>
             <Text fontSize={isSmallerThan768 ? '1.2rem' : '1.6rem'} mt={14} py={0}>
-              {USER_NAME}의 편지를 전달해주는 {isSmallerThan768 ? undefined : <br />} 머쓱이 5마리가 기다리고 있어요!
+              {user?.username}의 편지를 전달해주는 {isSmallerThan768 ? undefined : <br />} 머쓱이 5마리가 기다리고
+              있어요!
             </Text>
             <Button leftIcon={<AddIcon />} w={56} mt={4} colorScheme="primary" onClick={goToNewPost}>
               새로운 머쓱이 추가
             </Button>
           </Stack>
           <Grid gridTemplateColumns={isSmallerThan768 ? '1fr' : 'repeat(3, 1fr)'} gap={5} p={6} justifyItems={'center'}>
-            {DUMMY_DATA.map((card) => (
-              <GridItem key={card.id}>
+            {postList?.map((post) => (
+              <GridItem key={post._id}>
                 <PostCard
-                  {...card}
-                  handleClick={() => {
-                    navigate(`/post/${card.id}`);
+                  imgUrl={post.musseukImageName}
+                  musseukName={post.title}
+                  musseukContent={post.content}
+                  letter={post.comments.length}
+                  onClick={() => {
+                    navigate(`/post/${post._id}`);
                   }}
                 />
               </GridItem>
