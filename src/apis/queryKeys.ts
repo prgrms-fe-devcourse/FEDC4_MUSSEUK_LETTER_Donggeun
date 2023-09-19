2110af2a0ee35389db6d1d06ab37edd182c12f95
bbearcookie <aodem@naver.com>
2023-09-20 02:11:22 +0900

    refactor: 슬랙 queryKey 추가 및 각 id에 명시적인 이름 부여

--- src/apis/queryKeys.ts
+++ src/apis/queryKeys.ts
@@ -1,13 +1,17 @@
 const queryKey = {
   auth: ['auth'] as const,
+  slack: {
+    all: ['slack'] as const,
+    token: (token: string) => ['slack', token] as const
+  },
   users: {
     all: ['users'] as const,
-    detail: (id: number) => ['users', id] as const,
+    detail: (userId: number) => ['users', userId] as const,
     search: (keyword: string) => ['users', 'search', keyword] as const
   },
   posts: {
     all: ['posts'] as const,
-    detail: (id: string) => ['posts', id] as const,
+    detail: (postId: string) => ['posts', postId] as const,
     search: (userId: string) => ['posts', 'search', userId] as const
   }
 };
