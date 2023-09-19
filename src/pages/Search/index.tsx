b2acf69b5850a88f759f73f38302a38555f24aff
juyeon-park <lpjy1107@gmail.com>
2023-09-20 00:39:44 +0900

    refactor: searchPage line 수정

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -10,12 +10,12 @@ const Search = () => {
   return (
     <VStack spacing={12} mb={10}>
       <Box w="100%" bgGradient="linear-gradient(180deg, #C6FFC1 0%, #F5FFE2 100%)" p="3rem">
-        <Flex flexDirection={'column'} gap={4}>
-          <Heading mb={4}>데브코스 익명 편지 전송 서비스</Heading>
-          <Text fontSize="xl">팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요?</Text>
-          <Text fontSize="xl">다른 사람의 머쓱이에게 편지를 남겨보세요!</Text>
-          <Text fontSize="xl"> 또는, 당신의 머쓱이를 만들어서 공유해보세요! </Text>
-        </Flex>
+        <Heading mb={4}>데브코스 익명 편지 전송 서비스</Heading>
+        <br></br>
+        <Text fontSize="xl">팀원들에게 전하지 못한 말들이 있어 아쉽지 않으셨나요?</Text>
+        <Text fontSize="xl">다른 사람의 머쓱이에게 편지를 남겨보세요!</Text>
+        <br></br>
+        <Text fontSize="xl"> 또는, 당신의 머쓱이를 만들어서 공유해보세요! </Text>
       </Box>
       <HStack gap={4}>
         <Text fontWeight={'bold'} fontSize={'2xl'}>
