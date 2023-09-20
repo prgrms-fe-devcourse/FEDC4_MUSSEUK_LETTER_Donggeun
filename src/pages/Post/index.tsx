214531da2f7c448fdd2064d750115fef54a90160
Howon Shin <shinhw371@gmail.com>
2023-09-20 15:50:36 +0900

    feat: 포스트 상세 페이지 리다이렉션 및 댓글 권한 설정 (#113)
    * feat: 비로그인 redirection 추가
    
    * feat: 코멘트 작성 구역 강조 및 댓글 조회 권한 설정
    
    * feat: 404 페이지 연결
    
    * fix: 타입지정 누락 수정

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Heading, Text, VStack, useDisclosure } from '@chakra-ui/react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import AnnouncementText from './components/AnnouncementText';
 import ListButton from './components/ListButton';
 import DescriptionText from './components/DescriptionText';
@@ -12,16 +12,25 @@ import CommentListModal from './components/CommentListModal';
 import CommentInfoModal from './components/CommentInfoModal';
 import { CommentInfoProvider } from './contexts/CommentInfoProvider';
 
+const links = {
+  notFound: '/notFound',
+  signin: '/signin'
+};
+
 const Post = () => {
   const { postId = '' } = useParams();
 
   const { isOpen: isWriteOpen, onOpen: onWriteOpen, onClose: onWriteClose } = useDisclosure();
   const { isOpen: isInfoOpen, onOpen: onInfoOpen, onClose: onInfoClose } = useDisclosure();
   const { isOpen: isListOpen, onOpen: onListOpen, onClose: onListClose } = useDisclosure();
-  const { data: postData } = usePostDetailQuery(postId);
-  const { data: userData } = useAuthCheckQuery();
+  const { data: postData, isError: isPostError } = usePostDetailQuery(postId);
+  const { data: userData, isError: isUserError } = useAuthCheckQuery();
 
   const isAuthor = !!userData && !!postData && userData._id === postData.author._id;
+
+  if (isUserError) return <Navigate to={links.signin} />;
+
+  if (isPostError) return <Navigate to={links.notFound} />;
 
   return (
     <CommentInfoProvider>
@@ -43,12 +52,18 @@ const Post = () => {
                 개의 편지가 전달됐어요!
               </>
             ) : (
-              `원하는 위치를 클릭해서 ${postData?.author.username} 님의 머쓱이에게 편지를 남겨주세요.`
+              <>
+                원하는{' '}
+                <Text display={'inline'} color={'green03'}>
+                  위치
+                </Text>
+                를 클릭해서 {postData?.author.username} 님의 머쓱이에게 편지를 남겨주세요.
+              </>
             )}
           </AnnouncementText>
           {isAuthor && <ListButton onClick={onListOpen} />}
         </Box>
-        <CommentBoard onInfoOpen={onInfoOpen} onWriteOpen={onWriteOpen} postId={postId} />
+        <CommentBoard onInfoOpen={onInfoOpen} onWriteOpen={onWriteOpen} postId={postId} isAuthor={isAuthor} />
         <Heading mb="1rem">{postData?.title}</Heading>
         <DescriptionText>{postData?.content}</DescriptionText>
       </VStack>
