242fe5195e4c0d32d51a2ecf0bca3d12ce9e5ade
Howon Shin <shinhw371@gmail.com>
2023-09-21 00:10:53 +0900

    feat: 텍스트 suspense 작성

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,8 +1,7 @@
-import { Box, Heading, Text, VStack, useDisclosure } from '@chakra-ui/react';
+import { Box, SkeletonText, VStack, useDisclosure } from '@chakra-ui/react';
 import { Navigate, useParams } from 'react-router-dom';
 import AnnouncementText from './components/AnnouncementText';
 import ListButton from './components/ListButton';
-import DescriptionText from './components/DescriptionText';
 import BackgroundHome from '@/assets/images/background_home.png';
 import CommentBoard from './components/CommentBoard';
 import CommentWriteModal from './components/CommentWriteModal';
@@ -11,6 +10,9 @@ import useAuthCheckQuery from '@/apis/queries/useAuthCheckQuery';
 import CommentListModal from './components/CommentListModal';
 import CommentInfoModal from './components/CommentInfoModal';
 import { CommentInfoProvider } from './contexts/CommentInfoProvider';
+import { Suspense } from 'react';
+import Introduction from './components/Introduction';
+import IntroductionSkeleton from './components/Skeletons/IntroductionSkeleton';
 
 const links = {
   notFound: '/notFound',
@@ -23,6 +25,7 @@ const Post = () => {
   const { isOpen: isWriteOpen, onOpen: onWriteOpen, onClose: onWriteClose } = useDisclosure();
   const { isOpen: isInfoOpen, onOpen: onInfoOpen, onClose: onInfoClose } = useDisclosure();
   const { isOpen: isListOpen, onOpen: onListOpen, onClose: onListClose } = useDisclosure();
+
   const { data: postData, isError: isPostError } = usePostDetailQuery(postId);
   const { data: userData, isError: isUserError } = useAuthCheckQuery();
 
@@ -42,30 +45,15 @@ const Post = () => {
         backgroundSize="contain"
         backgroundRepeat="no-repeat">
         <Box w="100%">
-          <AnnouncementText mb="1rem">
-            {isAuthor ? (
-              <>
-                {postData?.author.username} 님에게{' '}
-                <Text display="inline" color="red.400">
-                  {postData?.comments.length}
-                </Text>
-                개의 편지가 전달됐어요!
-              </>
-            ) : (
-              <>
-                원하는{' '}
-                <Text display={'inline'} color={'green03'}>
-                  위치
-                </Text>
-                를 클릭해서 {postData?.author.username} 님의 머쓱이에게 편지를 남겨주세요.
-              </>
-            )}
-          </AnnouncementText>
-          {isAuthor && <ListButton onClick={onListOpen} />}
+          <Suspense fallback={<SkeletonText noOfLines={2} skeletonHeight={'2rem'} />}>
+            <AnnouncementText postId={postId} mb="1rem" />
+            {isAuthor && <ListButton onClick={onListOpen} />}
+          </Suspense>
         </Box>
         <CommentBoard onInfoOpen={onInfoOpen} onWriteOpen={onWriteOpen} postId={postId} isAuthor={isAuthor} />
-        <Heading mb="1rem">{postData?.title}</Heading>
-        <DescriptionText>{postData?.content}</DescriptionText>
+        <Suspense fallback={<IntroductionSkeleton />}>
+          <Introduction postId={postId} />
+        </Suspense>
       </VStack>
       <CommentWriteModal isOpen={isWriteOpen} onClose={onWriteClose} postId={postId} />
       <CommentInfoModal isOpen={isInfoOpen} onClose={onInfoClose} />
