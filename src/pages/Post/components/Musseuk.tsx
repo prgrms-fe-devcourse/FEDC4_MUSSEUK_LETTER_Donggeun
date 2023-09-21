2a62e34fcad7a0a1b8d5de37a76b82640b735f65
Howon Shin <shinhw371@gmail.com>
2023-09-21 15:15:05 +0900

    feat 포스트 상세 페이지 suspense 적용 (#126)
    * feat: 텍스트 suspense 작성
    
    * feat: 코멘트 보드 스켈레톤
    
    * feat: 머쓱이 이미지 기본 크기 설정
    
    * feat: useIsNotLoggedIn 사용

--- src/pages/Post/components/Musseuk.tsx
+++ src/pages/Post/components/Musseuk.tsx
@@ -9,8 +9,18 @@ type MusseukProps = {
 
 const Musseuk = forwardRef<HTMLImageElement, MusseukProps>(
   ({ musseukImageName, ...props }: MusseukProps, ref: ForwardedRef<HTMLImageElement>) => {
-    // TODO: 크기 기준점으로 삼고 있어서 로드 안됐을 때 크기 설정 필요함
-    return <Image src={MUSSEUK_IMAGE[musseukImageName]} alt="musseuk" w="90vw" maxW="45rem" ref={ref} {...props} />;
+    return (
+      <Image
+        src={MUSSEUK_IMAGE[musseukImageName]}
+        alt="musseuk"
+        w="90vw"
+        maxW="45rem"
+        h="90vw"
+        maxH="45rem"
+        ref={ref}
+        {...props}
+      />
+    );
   }
 );
 
