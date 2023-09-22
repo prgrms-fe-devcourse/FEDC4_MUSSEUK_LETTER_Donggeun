00c2b7a2ae99d6e84fed12de8c9396d471a8d991
from1to2 <ngcd12345@gmail.com>
2023-09-22 17:50:35 +0900

    style: 기존 hover 적용

--- src/pages/Newpost/components/MusseukItem.tsx
+++ src/pages/Newpost/components/MusseukItem.tsx
@@ -3,14 +3,7 @@ import { Image, Box } from '@chakra-ui/react';
 
 const MusseukItem = ({ ...props }: ComponentProps<typeof Image>) => {
   return (
-    <Box
-      // tabIndex={0}
-      flex="1"
-      border="0.3rem solid transparent"
-      borderRadius="10px"
-      _hover={{ borderColor: '#72D988' }}
-      // _focus={{ borderColor: '#72D988' }}
-    >
+    <Box flex="1" border="0.3rem solid transparent" borderRadius="10px" _hover={{ borderColor: '#72D988' }}>
       <Image objectFit="cover" w="100%" h="100%" {...props} />
     </Box>
   );
