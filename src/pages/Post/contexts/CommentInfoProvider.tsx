906358ed61f2fd63586b579e307a4d9508c2e7a2
Howon Shin <shinhw371@gmail.com>
2023-09-22 14:55:01 +0900

    feat: 댓글 삭제 기능 구현 (#154)
    * feat: 코멘트 삭제 mutation 작성
    
    * feat: 코멘트 삭제 기능 연결
    
    * design: 텍스트 정렬 수정
    
    * refactor: reducer action 수정

--- src/pages/Post/contexts/CommentInfoProvider.tsx
+++ src/pages/Post/contexts/CommentInfoProvider.tsx
@@ -1,13 +1,23 @@
-import { CommentField } from '@/types';
+import { Comment } from '@/types';
 import React, { useReducer } from 'react';
 import { COMMENT_INFO_ACTION } from '../constants';
 import { CommentInfoAction, CommentInfoContext, CommentInfoDispatchContext } from './CommentInfoContext';
 
-const initialCommentInfo: CommentField = {
+const initialCommentInfo: Comment = {
+  _id: '',
   content: '',
   position: [0, 0],
   nickname: '익명의 머쓱이',
-  decorationImageName: 'decoration_soju1'
+  decorationImageName: 'decoration_soju1',
+  author: {
+    _id: '',
+    email: '',
+    image: '',
+    postCount: 0,
+    commentCount: 0,
+    username: '',
+    introduce: ''
+  }
 };
 
 export const CommentInfoProvider = ({ children }: { children: React.ReactNode }) => {
@@ -20,27 +30,21 @@ export const CommentInfoProvider = ({ children }: { children: React.ReactNode })
   );
 };
 
-const commentInfoReducer = (state: CommentField, action: CommentInfoAction): CommentField => {
+const commentInfoReducer = (state: Comment, action: CommentInfoAction): Comment => {
   switch (action.type) {
-    case COMMENT_INFO_ACTION.CONTENT:
+    case COMMENT_INFO_ACTION.INFO:
       return {
         ...state,
-        content: action.content
+        _id: action._id,
+        author: action.author,
+        content: action.content,
+        nickname: action.nickname,
+        decorationImageName: action.decorationImageName
       };
     case COMMENT_INFO_ACTION.POSITION:
       return {
         ...state,
         position: action.position
-      };
-    case COMMENT_INFO_ACTION.NICKNAME:
-      return {
-        ...state,
-        nickname: action.nickname
-      };
-    case COMMENT_INFO_ACTION.DECORATION:
-      return {
-        ...state,
-        decorationImageName: action.decorationImageName
       };
     default:
       throw new Error('Unhandled action');
