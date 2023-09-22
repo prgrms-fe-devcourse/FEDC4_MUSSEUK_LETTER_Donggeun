3de1fbc8a58791f63db6e3582a64a23302cd2cfe
bbearcookie <aodem@naver.com>
2023-09-22 21:12:31 +0900

    chore: configs 디렉토리 마이그레이션

---
+++ packages/slack/src/app.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import app from '@/configs/express';
+// import router from '@/routes';
+import '@/configs/redis';
+
+const PORT = process.env.PORT;
+
+// app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running in ${PORT} port.`);
+});
