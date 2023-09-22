8f7260eb2dd75326cfdff3e21ca664d6eae0fd1e
bbearcookie <aodem@naver.com>
2023-09-23 04:47:18 +0900

    chore: app. main 마이그레이션

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,31 +1,40 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import useAuthCheckQuery from '@/apis/queries/useAuthCheckQuery';
+import SignIn from './pages/Signin';
+import SignUp from './pages/Signup';
+import MainLayout from './routes/layout';
+import Main from './pages/Main';
+import Profile from './pages/Profile';
+import NewPost from './pages/Newpost';
+import Post from './pages/Post';
+import Search from './pages/Search';
+import ChangePassword from './pages/ChangePassword';
+import SettingSlack from './pages/Setting/Slack';
+import NotFound from './pages/NotFound';
+import SlackConfirmationWrapper from './pages/Setting/SlackConfirmation';
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  useAuthCheckQuery();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Router>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Main />} />
+          <Route path="/post/:postId" element={<Post />} />
+          <Route path="/newpost" element={<NewPost />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+          <Route path="/search/:keyword" element={<Search />} />
+          <Route path="/setting/slack" element={<SettingSlack />} />
+          <Route path="/changePassword" element={<ChangePassword />} />
+          <Route path="/setting/slack/confirmation" element={<SlackConfirmationWrapper />} />
+        </Route>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import queryClient from '@/apis/queryClient.ts';
+import theme from '@/theme/index.tsx';
+import Fonts from '@/theme/font.tsx';
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <App />
+        <Fonts />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
