f7fa0b1deb4add1772c9668dd8aef6fb702d976a
bbearcookie <aodem@naver.com>
2023-09-22 21:00:06 +0900

    init: 프로젝트 파일 삭제

--- src/pages/Signup/helpers/password.ts
+++
@@ -1,7 +0,0 @@
-export const isPasswordTooShort = (password: string) => {
-  return !password || password.length < 8;
-};
-
-export const isPasswordContainNumber = (password: string) => {
-  return /\d/.test(password);
-};
--- src/pages/Signup/templates/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as LinkTemplate } from './LinkTemplate';
