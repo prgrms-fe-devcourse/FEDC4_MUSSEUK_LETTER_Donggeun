380b4305f05742f782e892a482e798f1c75ff69d
Howon-Shin <shinhw371@naver.com>
2023-09-26 01:14:52 +0900

    fix: 메인페이지 머쓱이 이미지 나가는 버그 수정 및 스타일 정리

--- packages/web/src/pages/Main/components/MusseukImage.tsx
+++ packages/web/src/pages/Main/components/MusseukImage.tsx
@@ -19,18 +19,18 @@ const MusseukImage = () => {
         }}
         h={{
           base: 'auto',
-          lg: '30rem'
+          lg: '24rem'
         }}
         top="8rem"
         right="1rem"
+        maxW={'24rem'}
         src={musseuk_semicolon}
         alt="musseuk_semicolon"
       />
       <Box
         height={{
           base: '0',
-          lg: '10rem',
-          '2xl': '0'
+          lg: '5rem'
         }}
       />
     </>
