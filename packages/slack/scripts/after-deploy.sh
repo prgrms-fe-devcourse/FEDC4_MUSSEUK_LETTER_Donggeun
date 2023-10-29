e5180554f628d0cac82a525e9212294abbd8c8eb
bbearcookie <aodem@naver.com>
2023-10-29 17:01:20 +0900

    chore: after-deploy 수정

--- packages/slack/scripts/after-deploy.sh
+++ packages/slack/scripts/after-deploy.sh
@@ -3,10 +3,6 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo /usr/bin/yarn
 
-sudo yarn
-
-sudo pm2 start dist
+sudo /usr/bin/pm2 start dist
