85f0114e8cb70c8cfb9ce02c0ed861a234260820
bbearcookie <aodem@naver.com>
2023-10-29 16:57:33 +0900

    chore: nvm 경로 작성

--- packages/slack/scripts/after-deploy.sh
+++ packages/slack/scripts/after-deploy.sh
@@ -3,6 +3,10 @@ REPOSITORY=/home/ubuntu/build
 
 cd $REPOSITORY
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 sudo yarn
 
 sudo pm2 start dist
