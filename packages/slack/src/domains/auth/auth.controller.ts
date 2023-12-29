3246458adda277eb78f8ceadd28246d3466003db
bbearcookie <aodem@naver.com>
2023-12-30 03:55:27 +0900

    feat: signup validation 정리

--- packages/slack/src/domains/auth/auth.controller.ts
+++ packages/slack/src/domains/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import { validator } from '@/utils/validator';
 import authValidator from '@/domains/auth/auth.validator';
 
--- packages/slack/src/domains/auth/auth.validator.ts
+++ packages/slack/src/domains/auth/auth.validator.ts
@@ -10,13 +10,19 @@ const authValidator: Validator = {
           required_error: '아이디를 입력해주세요.'
         })
         .min(1, '아이디를 입력해주세요.')
-        .max(6, '최대 6글자'),
+        .max(20, '아이디는 최대 20글자까지만 입력할 수 있습니다.'),
       password: z
         .string({
           required_error: '비밀번호를 입력해주세요.'
         })
-        .min(1, '비밀번호를 입력해주세요.')
-        .max(6, '최대 6글자')
+        .min(8, '8글자 이상으로만 입력할 수 있습니다.')
+        .refine((value) => /\d/.test(value), '숫자를 반드시 포함해야 합니다.'),
+      name: z
+        .string({
+          required_error: '실명을 입력해주세요.'
+        })
+        .min(2, '실명을 최소 2글자 이상으로만 입력할 수 있습니다.')
+        .max(4, '실명은 최대 4글자까지만 입력할 수 있습니다.')
     })
   }
 };
