de97164aa1a9330cfbb1c3494c214dfe19b4cabc
bbearcookie <aodem@naver.com>
2023-12-30 05:53:16 +0900

    feat: 각 도메인별 디렉토리 구조 구성

--- packages/slack/src/domains/auth/auth.controller.ts
+++ packages/slack/src/domains/auth/auth.controller.ts
@@ -8,27 +8,27 @@ const router = express.Router();
 router.post('/signup', validationFilter(authValidator.signup), (req, res) => {
   const body = req.body as z.infer<typeof authValidator.signup.body>;
 
-  res.json({ message: '회원가입 구현 필요..', body });
+  res.json({ message: 'TODO: 회원가입 구현 필요..', body });
 });
 
 router.post('/signin', validationFilter(authValidator.signin), (req, res) => {
   const body = req.body as z.infer<typeof authValidator.signin.body>;
 
-  res.json({ message: '로그인 구현 필요..', body });
+  res.json({ message: 'TODO: 로그인 구현 필요..', body });
 });
 
 router.post('/signout', (req, res) => {
-  res.json({ message: '로그아웃 구현 필요..' });
+  res.json({ message: 'TODO: 로그아웃 구현 필요..' });
 });
 
 router.get('/check', (req, res) => {
-  res.json({ message: '로그인 상태 확인 구현 필요..' });
+  res.json({ message: 'TODO: 로그인 상태 확인 구현 필요..' });
 });
 
 router.put('/password', validationFilter(authValidator.password), (req, res) => {
   const body = req.body as z.infer<typeof authValidator.password.body>;
 
-  res.json({ message: '비밀번호 변경 구현 필요..', body });
+  res.json({ message: 'TODO: 비밀번호 변경 구현 필요..', body });
 });
 
 export default router;
--- packages/slack/src/router.ts
+++ packages/slack/src/router.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express';
 import authController from '@/domains/auth/auth.controller';
+import commentsController from '@/domains/comments/comments.controller';
+import postsController from '@/domains/posts/posts.controller';
+import usersController from '@/domains/users/users.controller';
 import { ResponseError, ValidationError, AuthorizationError } from '@/utils/ResponseError';
 
 const router = express.Router();
 
 router.use('/auth', authController);
+router.use('/comments', commentsController);
+router.use('/posts', postsController);
+router.use('/users', usersController);
 
 /**
  * @NOTE 앞선 미들웨어에서 오류 발생시 클라이언트에게 오류 응답을 전달합니다.
