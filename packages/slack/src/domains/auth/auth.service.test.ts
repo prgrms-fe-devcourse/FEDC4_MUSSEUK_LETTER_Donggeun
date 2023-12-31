5a68f58383d2d6711754a638104da5b1dd105518
bbearcookie <aodem@naver.com>
2023-12-31 18:40:40 +0900

    style: 텍스트 변경

--- packages/slack/src/domains/auth/auth.service.test.ts
+++ packages/slack/src/domains/auth/auth.service.test.ts
@@ -14,13 +14,13 @@ describe('회원가입', () => {
     expect(user.userId).toBe(mockUser.id);
   });
 
-  it('이미 존재하는 회원일 경우 예외 발생', async () => {
+  it('이미 존재하는 회원일 경우 예외가 발생한다', async () => {
     UserRepository.findOneBy = jest.fn().mockResolvedValueOnce(mockUser);
 
     expect(authService.signUp(mockUser.username, mockUser.password, mockUser.name)).rejects.toThrow();
   });
 
-  it('생성된 유저 정보를 찾을 수 없을 경우 예외 발생', async () => {
+  it('생성된 유저 정보를 찾을 수 없을 경우 예외가 발생한다', async () => {
     UserRepository.findUserByUsername = jest.fn().mockResolvedValueOnce(null);
 
     expect(authService.signUp(mockUser.username, mockUser.password, mockUser.name)).rejects.toThrow();
@@ -38,13 +38,13 @@ describe('로그인', () => {
     expect(user.userId).toBe(mockUser.id);
   });
 
-  it('존재하지 않는 회원일 경우 예외 발생', async () => {
+  it('존재하지 않는 회원일 경우 예외가 발생한다', async () => {
     UserRepository.findOneBy = jest.fn().mockResolvedValueOnce(null);
 
     expect(authService.signIn('아이디', '12341234')).rejects.toThrow();
   });
 
-  it('비밀번호가 일치하지 않을 경우 예외 발생', async () => {
+  it('비밀번호가 일치하지 않을 경우 예외가 발생한다', async () => {
     UserRepository.findOneBy = jest.fn().mockResolvedValueOnce(mockUser);
 
     expect(authService.signIn('아이디', '11112222')).rejects.toThrow();
@@ -62,7 +62,7 @@ describe('로그인 확인', () => {
     expect(user.userId).toBe(mockUser.id);
   });
 
-  it('DB에 유저 정보가 없을 경우 예외 발생', async () => {
+  it('DB에 유저 정보가 없을 경우 예외가 발생한다', async () => {
     UserRepository.findOneBy = jest.fn().mockResolvedValueOnce(null);
 
     expect(authService.signCheck(mockUser.id)).rejects.toThrow();
@@ -70,7 +70,7 @@ describe('로그인 확인', () => {
 });
 
 describe('비밀번호 변경', () => {
-  it('DB에 유저 정보가 없을 경우 예외 발생', async () => {
+  it('DB에 유저 정보가 없을 경우 예외가 발생한다', async () => {
     UserRepository.findOneBy = jest.fn().mockResolvedValueOnce(null);
 
     expect(authService.changePassword(1, '12341234')).rejects.toThrow();
